816518b7b4970f5d2706455ac2ec962908ceea44
Natan Sousa <natanslsousa.16@gmail.com>
2024-10-02 14:03:14 -0300

    Alterações de mais em tão pouco tempo para catalogar

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() {
 const appTitle = 'Bem vinda, Joice';
 const leisPublicas = 'LEIS PÚBLICAS';
 const instituicoesApoio = 'INSTITUIÇÕES DE APOIO';
-const numerosEmergencia = 'NÚMEROS DE EMERGÊNCIA';
+const numerosEmergencia = 'NÚMEROS EMERGÊNCIAIS';
 const funcao = 'FUNÇÃO';
 const sos = 'SOS';
 
@@ -65,39 +65,42 @@ class HomePage extends StatelessWidget {
               Text(
                 appTitle,
                 style: TextStyle(
-                  fontSize: 28,
+                  fontSize: 30,
                   fontWeight: FontWeight.bold,
                   color: Colors.white,
                 ),
               ),
               SizedBox(height: 50),
               MenuButton(
-                iconPath: 'assets/estrela.png',
+                iconPath: 'assets/leis_publicas.png',
                 label: leisPublicas,
                 onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => LeisPublicasPage())),
               ),
               MenuButton(
-                iconPath: 'assets/estrela.png',
+                iconPath: 'assets/instituicoes_apoio.png',
                 label: instituicoesApoio,
                 onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => InstituicoesApoioPage())),
               ),
               MenuButton(
-                iconPath: 'assets/estrela.png',
+                iconPath: 'assets/numeros_emergencia.png',
                 label: numerosEmergencia,
                 onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => NumerosEmergenciaPage())),
               ),
-              MenuButton(
-                iconPath: 'assets/estrela.png',
-                label: funcao,
-                onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => FuncaoPage())),
-              ),
               Spacer(),
-              MenuButton(
-                iconPath: 'assets/adicao.png',
-                label: sos,
-                backgroundColor: Color(0xFFFFD600),
-                textColor: Colors.blue,
-                onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => SOSPage())),
+              Align(
+                alignment: Alignment.bottomCenter,  // Centraliza o botão na parte inferior
+                child: Container(
+                  margin: EdgeInsets.only(bottom: 20),  // Margem inferior
+                  width: 200,  // Diminui o tamanho do botão SOS
+                  child: MenuButton(
+                    iconPath: 'assets/sos.png',
+                    label: sos,
+                    backgroundColor: Color(0xFFFFD600),
+                    textColor: Colors.blue,
+                    hideArrow: true,  // Remove a seta
+                    onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => SOSPage())),
+                  ),
+                ),
               ),
             ],
           ),
@@ -113,6 +116,7 @@ class MenuButton extends StatelessWidget {
   final Color backgroundColor;
   final Color textColor;
   final VoidCallback onPressed;
+  final bool hideArrow;  // Propriedade para esconder a seta
 
   MenuButton({
     required this.iconPath,
@@ -120,6 +124,7 @@ class MenuButton extends StatelessWidget {
     required this.onPressed,
     this.backgroundColor = Colors.white,
     this.textColor = Colors.deepPurple,
+    this.hideArrow = false,  // Define padrão como falso, seta visível
   });
 
   @override
@@ -137,19 +142,28 @@ class MenuButton extends StatelessWidget {
             borderRadius: BorderRadius.circular(10),
           ),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              SizedBox(width: 16),
-              Image.asset(iconPath, width: 24, height: 24),
-              SizedBox(width: 16),
-              Text(
-                label,
-                style: TextStyle(
-                  fontSize: 18,
-                  color: textColor,
-                  fontWeight: FontWeight.bold,
-                ),
+              Row(
+                children: [
+                  SizedBox(width: 16),
+                  Image.asset(iconPath, width: 24, height: 24),  // Ícone à esquerda
+                  SizedBox(width: 16),
+                  Text(
+                    label,
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: textColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
+              if (!hideArrow)  // Mostra a seta somente se hideArrow for falso
+                Padding(
+                  padding: const EdgeInsets.only(right: 16.0),
+                  child: Image.asset('assets/seta.png', width: 24, height: 24),  // Seta à direita
+                ),
             ],
           ),
         ),
@@ -163,33 +177,259 @@ class LeisPublicasPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Background(
-        child: Center(
-          child: Text(
-            'Conteúdo da página de $leisPublicas',
-            style: TextStyle(color: Colors.white),
+      backgroundColor: Colors.purpleAccent, // Cor de fundo da tela
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            // LEI MARIA DA PENHA
+            customExpansionTile(
+              icon: Icons.gavel,
+              title: 'LEI MARIA DA PENHA',
+              description: 'O que é?\n\n'
+                  'Lei brasileira que visa proteger as mulheres da violência doméstica e familiar. A lei define a violência doméstica como crime, e aponta formas de evitar, enfrentar e punir a agressão.',
+              additionalInfo: 'O que precisa?\n\n'
+                  '1. Procurar órgãos públicos como saúde, assistência social, segurança pública, e justiça.\n'
+                  '2. Não tenha vergonha de pedir ajuda, inclusive psicológica.\n'
+                  '3. Ligue 180 para atendimento 24 horas.\n'
+                  '4. Solicite medidas protetivas para afastamento do agressor.',
+            ),
+            SizedBox(height: 20),
+
+            // MEDIDA PROTETIVA ONLINE
+            customExpansionTile(
+              icon: Icons.shield,
+              title: 'MEDIDA PROTETIVA ONLINE',
+              description: 'O que é?\n\n'
+                  'Medida criada por lei e determinada pelo juiz, para proteger mulheres vítimas de violência doméstica e familiar.',
+              additionalInfo: 'O que precisa?\n\n'
+                  '1. Acessar o gov.br com seu CPF.\n'
+                  '2. Preencher um formulário descrevendo o caso.\n'
+                  '3. Aguardar a aprovação.',
+            ),
+            SizedBox(height: 20),
+
+            // BOLSA FAMÍLIA
+            customExpansionTile(
+              icon: Icons.family_restroom,
+              title: 'BOLSA FAMÍLIA',
+              description: 'O que é?\n\n'
+                  'Programa de transferência de renda para famílias com renda mensal de, no máximo, 218 reais por pessoa.',
+              additionalInfo: 'Como fazer?\n\n'
+                  '1. Estar inscrito no Cadastro Único, com dados atualizados.\n'
+                  '2. O cadastramento é feito no CRAS, com apresentação de CPF ou título de eleitor.',
+            ),
+            SizedBox(height: 20),
+
+            // VALE GÁS
+            customExpansionTile(
+              icon: Icons.local_gas_station,
+              title: 'VALE GÁS',
+              description: 'O que é?\n\n'
+                  'Recarga gratuita de botijão de gás para famílias cearenses em situação de vulnerabilidade, distribuída três vezes ao ano.',
+              additionalInfo: 'Quem tem direito?\n\n'
+                  '1. Famílias com Cartão Mais Infância Ceará ou inscritas no CadÚnico, beneficiadas pelo Bolsa Família.',
+            ),
+            SizedBox(height: 20),
+
+            // BPC - BENEFÍCIO DE PRESTAÇÃO CONTINUADA
+            customExpansionTile(
+              icon: Icons.attach_money,
+              title: 'BPC - BENEFÍCIO DE PRESTAÇÃO CONTINUADA',
+              description: 'O que é?\n\n'
+                  'Benefício de um salário mínimo pago mensalmente a idosos ou pessoas com deficiência que não podem garantir sua sobrevivência.',
+              additionalInfo: 'Quem tem direito?\n\n'
+                  '1. Pessoa idosa com 65 anos ou mais e renda familiar de até 1/4 do salário mínimo por pessoa.\n'
+                  '2. O Cadastro Único deve estar atualizado há menos de dois anos.',
+            ),
+            SizedBox(height: 20),
+
+            // CARTÃO MAIS INFÂNCIA CEARÁ
+            customExpansionTile(
+              icon: Icons.card_giftcard,
+              title: 'CARTÃO MAIS INFÂNCIA CEARÁ',
+              description: 'O que é?\n\n'
+                  'Programa do Governo do Ceará que oferece 100 Reais mensais para famílias em situação de vulnerabilidade social.',
+              additionalInfo: 'Quem tem direito?\n\n'
+                  '1. Famílias com renda per capita de até 89,00 Reais.',
+            ),
+            SizedBox(height: 20),
+
+            // CARTÃO CEARÁ SEM FOME
+            customExpansionTile(
+              icon: Icons.fastfood,
+              title: 'CARTÃO CEARÁ SEM FOME',
+              description: 'O que é?\n\n'
+                  'Programa que distribui 300 Reais mensais para compra de alimentos a famílias vulneráveis.',
+              additionalInfo: 'Quem tem direito?\n\n'
+                  '1. Famílias cadastradas no Cadastro Único há no máximo 24 meses.\n'
+                  '2. Beneficiárias do Bolsa Família, com renda per capita de até 218,00 Reais.',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget customExpansionTile({required IconData icon, required String title, required String description, required String additionalInfo}) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white, // Fundo branco
+        borderRadius: BorderRadius.circular(15), // Bordas arredondadas
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 4,
+            offset: Offset(2, 2), // Sombra suave
           ),
+        ],
+      ),
+      child: ExpansionTile(
+        leading: Icon(icon, color: Colors.purple), // Ícone à esquerda
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.purple, // Texto roxo
+          ),
+        ),
+        trailing: Icon(Icons.arrow_forward, color: Colors.amber), // Seta à direita
+        children: [
+          ListTile(
+            title: Text(
+              description,
+              style: TextStyle(color: Colors.black), // Texto preto
+            ),
+          ),
+          ListTile(
+            title: Text(
+              additionalInfo,
+              style: TextStyle(color: Colors.black), // Texto preto
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class InstituicoesApoioPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purpleAccent,  // Cor de fundo igual à página de Leis Públicas
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            SupportGroupItem(
+              icon: Icons.group,
+              title: 'Grupo terapêutico de saúde mental: Primazia',
+              description: 'Para mulheres das UBS Sede I e III',
+            ),
+            SizedBox(height: 20),
+            SupportGroupItem(
+              icon: Icons.group,
+              title: 'Grupo terapêutico de saúde mental: Florescer',
+              description: 'Para agentes de saúde da Sede',
+            ),
+            SizedBox(height: 20),
+            SupportGroupItem(
+              icon: Icons.group,
+              title: 'Grupo terapêutico de saúde mental: Amanhecer',
+              description: 'Para agentes de saúde da Lagoinha',
+            ),
+            SizedBox(height: 20),
+            SupportGroupItem(
+              icon: Icons.family_restroom,
+              title: 'Grupo terapêutico de saúde mental: Gerando Vidas',
+              description: 'Para gestantes da Sede I e III',
+            ),
+            SizedBox(height: 20),
+            SupportGroupItem(
+              icon: Icons.fitness_center,
+              title: 'Grupos de atividades físicas',
+              description: 'Vinculados às UBS',
+            ),
+            SizedBox(height: 20),
+            SupportGroupItem(
+              icon: Icons.child_friendly,
+              title: 'Grupo de mães atípicas no CIR',
+              description: 'Centro de Reabilitação',
+            ),
+            SizedBox(height: 20),
+            SupportGroupItem(
+              icon: Icons.self_improvement,
+              title: 'Grupo semanal de autocuidado no CAPS',
+              description: 'Com enfermeiro e assistente social',
+            ),
+            SizedBox(height: 20),
+            SupportGroupItem(
+              icon: Icons.psychology,
+              title: 'Grupo quinzenal de psicoterapia no CAPS',
+              description: 'E grupo mensal de práticas integrativas',
+            ),
+            SizedBox(height: 20),
+            SupportGroupItem(
+              icon: Icons.public,
+              title: 'Projeto Equivida',
+              description: 'Territórios: Lagoinha, Ubaia, Bom Sucesso, e mais',
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-class InstituicoesApoioPage extends StatelessWidget {
+// Componente para criar os itens dos grupos de apoio
+class SupportGroupItem extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String description;
+
+  SupportGroupItem({
+    required this.icon,
+    required this.title,
+    required this.description,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Background(
-        child: Center(
-          child: Text(
-            'Conteúdo da página de $instituicoesApoio',
-            style: TextStyle(color: Colors.white),
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,  // Fundo branco
+        borderRadius: BorderRadius.circular(15),  // Bordas arredondadas
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 4,
+            offset: Offset(2, 2),  // Sombra suave
+          ),
+        ],
+      ),
+      child: ListTile(
+        leading: Icon(icon, color: Colors.purple),  // Ícone à esquerda
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.purple,  // Texto roxo
           ),
         ),
+        subtitle: Text(
+          description,
+          style: TextStyle(color: Colors.black),  // Texto preto
+        ),
+        trailing: Icon(Icons.arrow_forward, color: Colors.amber),  // Seta à direita
       ),
     );
   }
 }
+
 
 class NumerosEmergenciaPage extends StatelessWidget {
   @override
